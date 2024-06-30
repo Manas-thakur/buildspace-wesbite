097169b9287d8014dfd274394778f33b39297e67
AayushhGoyal <agayush088@gmail.com>
2024-06-30 19:21:24 +0530

    timeline without animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,106 @@
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="font-sans m-0 p-0 h-full text-white bg-slate-500 min-h-screen overflow-x-hidden">
+      <div className="text-7xl m-auto m-auto pl-9 pt-8 pb-[60px] font-semibold ">
+        Smooth Operators
+      </div>
+      <div className="text-center p-[4rem] m-auto max-w-[600px]">
+        <h1 className="text-4xl mb-8">Our Timeline</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          quis expedita nobis! Voluptatem possimus nisi animi fugit ex non
+          doloremque accusantium nulla quia? Laudantium corporis animi, autem
+          maiores nostrum ratione.
+        </p>
+      </div>
+
+      <div className="relative min-h-[150px]">
+        <div className="relative line"></div>
+        <div className="bg-gray-800 p-4 rounded-lg shadow-md text-white w-[436px] ml-10 mt-4 ">
+          <div className="absolute block h-4 w-4 rounded-md bg-white left-[50%] top-[20%] translate-x-[-50%]"></div>
+          <div className="w-full">
+            <h2 className="font-semibold text-2xl">week 1</h2>
+            <p className="text-white">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est
+              facere, obcaecati voluptatum quis nemo facilis perferendis veniam
+              necessitatibus quo enim laborum excepturi et, quidem, consequuntur
+              in explicabo possimus sapiente minima?
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-800 p-4 rounded-lg shadow-md text-white w-[436px] ml-10 mt-4 relative left-[950px]">
+          <div className="absolute block h-4 w-4 rounded-md bg-white left-[50%] top-[20%] translate-x-[-50%]"></div>
+          <div className="w-full">
+            <h2 className="font-semibold text-2xl">week 1</h2>
+            <p className="text-white">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est
+              facere, obcaecati voluptatum quis nemo facilis perferendis veniam
+              necessitatibus quo enim laborum excepturi et, quidem, consequuntur
+              in explicabo possimus sapiente minima?
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-800 p-4 rounded-lg shadow-md text-white w-[436px] ml-10 mt-4 ">
+          <div className="absolute block h-4 w-4 rounded-md bg-white left-[50%] top-[20%] translate-x-[-50%]"></div>
+          <div className="w-full">
+            <h2 className="font-semibold text-2xl">week 1</h2>
+            <p className="text-white">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est
+              facere, obcaecati voluptatum quis nemo facilis perferendis veniam
+              necessitatibus quo enim laborum excepturi et, quidem, consequuntur
+              in explicabo possimus sapiente minima?
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-800 p-4 rounded-lg shadow-md text-white w-[436px] ml-10 mt-4 relative left-[950px]">
+          <div className="absolute block h-4 w-4 rounded-md bg-white left-[50%] top-[20%] translate-x-[-50%]"></div>
+          <div className="w-full">
+            <h2 className="font-semibold text-2xl">week 1</h2>
+            <p className="text-white">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est
+              facere, obcaecati voluptatum quis nemo facilis perferendis veniam
+              necessitatibus quo enim laborum excepturi et, quidem, consequuntur
+              in explicabo possimus sapiente minima?
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-800 p-4 rounded-lg shadow-md text-white w-[436px] ml-10 mt-4 ">
+          <div className="absolute block h-4 w-4 rounded-md bg-white left-[50%] top-[20%] translate-x-[-50%]"></div>
+          <div className="w-full">
+            <h2 className="font-semibold text-2xl">week 1</h2>
+            <p className="text-white">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est
+              facere, obcaecati voluptatum quis nemo facilis perferendis veniam
+              necessitatibus quo enim laborum excepturi et, quidem, consequuntur
+              in explicabo possimus sapiente minima?
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-800 p-4 rounded-lg shadow-md text-white w-[436px] ml-10 mt-4 relative left-[950px]">
+          <div className="absolute block h-4 w-4 rounded-md bg-white left-[50%] top-[20%] translate-x-[-50%]"></div>
+          <div className="w-full">
+            <h2 className="font-semibold text-2xl">week 1</h2>
+            <p className="text-white">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est
+              facere, obcaecati voluptatum quis nemo facilis perferendis veniam
+              necessitatibus quo enim laborum excepturi et, quidem, consequuntur
+              in explicabo possimus sapiente minima?
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-800 p-4 rounded-lg shadow-md text-white w-[436px] ml-10 mt-4 ">
+          <div className="absolute block h-4 w-4 rounded-md bg-white left-[50%] top-[20%] translate-x-[-50%]"></div>
+          <div className="w-full">
+            <h2 className="font-semibold text-2xl">week 1</h2>
+            <p className="text-white">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est
+              facere, obcaecati voluptatum quis nemo facilis perferendis veniam
+              necessitatibus quo enim laborum excepturi et, quidem, consequuntur
+              in explicabo possimus sapiente minima?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { isMinusToken, transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,7 +7,70 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'half-minus-1': 'calc(50% - 1px)',
+        'half-minus-2': 'calc(50% - 2rem)',
+        'ok': '600ms ease',
+        '1': 1,
+      }
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.line': {
+          position: 'absolute',
+          zIndex: 2,
+          left: 'calc(50% - 1px)',
+          width: '2px',
+          top: '50px',
+          bottom: '-50px',
+          backgroundColor: "white",
+          // display: 'none',
+        },
+        // '.line::before': {
+        //   position: 'absolute',
+        //   display: 'block',
+        //   content: '""',
+        //   height: '1rem',
+        //   width: '1rem',
+        //   borderRadius: '50%',
+        //   backgroundColor: '#000',
+        // },
+        '.line::after': {
+          position: 'absolute',
+          display: 'block',
+          content: '',
+          height: '1rem',
+          width: '1rem',
+          bottom: 0,
+          borderRadius: '50%',
+          backgroundColor: 'white',
+          transform: 'translate(-50%)',
+        },
+        // '.section':{
+        //   display: 'flex',
+        //   transform: 'translateX(-100%)',
+        //   transition: "600ms ease",
+        //   position: 'relative',
+        //   zIndex: 1,
+        //   margin: '50px 0',
+        //   padding: '1rem',
+        
+        //   borderRadius: '1rem',
+        //   alignitems: 'center',
+        //   minHeight: '300px',
+        // },
+
+        // '.section:nth-child(odd)':{
+        //   flexDirection: 'row-reverse',
+        //   transform: 'translateX(100%)',
+        // },
+      };
+      
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
+};
