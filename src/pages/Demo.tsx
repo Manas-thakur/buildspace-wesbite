430ad35fd169cd9ee57370ceba074d6837fd2924
Pulkit <kpulkit15234@gmail.com>
2024-07-01 12:00:55 +0530

    adding prettier

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -3,7 +3,7 @@ import Form from "../components/Form";
 export default function Demo() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-        <Form />
+      <Form />
     </div>
-  )
+  );
 }
