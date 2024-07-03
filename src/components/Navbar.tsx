1903994f592381ee314f76583211609820b37655
Pulkit <kpulkit15234@gmail.com>
2024-07-03 11:33:45 +0530

    navbar updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav
@@ -6,15 +8,12 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-3xl font-bold">Smooth Operator</div>
         <div>
-          <a href="#home" className="px-4">
+          <Link to="/" className="px-4">
             Home
-          </a>
-          <a href="#about" className="px-4">
-            About
-          </a>
-          <a href="#contact" className="px-4">
-            Contact
-          </a>
+          </Link>
+          <Link to="/demo" className="px-4">
+            Demo
+          </Link>
         </div>
       </div>
     </nav>
